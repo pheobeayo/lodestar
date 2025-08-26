9dfb06f1d4437aa58d57de905982b15307450873
Nazar Hussain <nazarhussain@gmail.com>
2025-08-26 12:12:37 +0200

    Use the batch operation on single repository

--- packages/db/src/controller/index.ts
+++ packages/db/src/controller/index.ts
@@ -1,3 +1,11 @@
-export type {Db, DbReqOpts, DatabaseController, FilterOptions, KeyValue} from "./interface.js";
+export type {
+  Db,
+  DbReqOpts,
+  DatabaseController,
+  FilterOptions,
+  KeyValue,
+  DbBatch,
+  DbBatchOperation,
+} from "./interface.js";
 export {LevelDbController} from "./level.js";
 export type {LevelDbControllerMetrics} from "./metrics.js";
